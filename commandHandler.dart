87ff35f4be8964dfb2b110ec9b881d5c03506a2e
Ozoid <steve@ozoid.co.uk>
2022-09-07 23:42:30 +0100

    Create commandHandler.dart

---
+++ commandHandler.dart
@@ -0,0 +1,38 @@
+import 'dart:core';
+import 'dart:developer';
+import 'messageHandler.dart';
+
+class CommandHandler{
+//-------------------------------------  
+  void doCommand(Message message){
+    switch (message.messageType){
+      case MessageType.GAME_START:
+        //startGame(message);
+        break;
+      case MessageType.LOGIN:
+        //loginUser(message);
+        break;
+      case MessageType.UNKNOWN:
+        //do something
+        break;
+      case MessageType.VOID_GAME:
+        //do something else
+        break;
+      default:
+        break;
+    }
+  }
+//-------------------------------------  
+  void startGame(Message msg){
+    log("Game Started");    
+  }
+//-------------------------------------  
+  void loginUser(Message msg){
+    if(msg.text == "Success"){
+      log("User Logged in");
+      return;
+    }
+    log("User Failed to login");
+  }
+//-------------------------------------  
+}
